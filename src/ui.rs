54fbba2325a59d7fa8254a403bc70624c1e9f39c
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2024-05-14 15:17:28 +0200

    .

--- src/ui.rs
+++ src/ui.rs
@@ -3,7 +3,7 @@ use ratatui::{style::Stylize, widgets::Paragraph, Frame};
 pub fn ui(f: &mut Frame) {
     let area = f.size();
     f.render_widget(
-        Paragraph::new("Hello Ratatui! (press 'q' to quit)")
+        Paragraph::new("Hello Ratatui! (press 'q' to quit).")
             .white()
             .on_blue(),
         area,
